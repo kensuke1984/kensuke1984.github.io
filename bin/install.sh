3ced69040324223200e107984897dee97b827811
kensuke1984 <kensuke@earth.sinica.edu.tw>
2016-07-21 17:42:09 +0800

    0721

--- bin/install.sh
+++ bin/install.sh
@@ -1,57 +1,73 @@
-#!/bin/sh
+#!/bin/bash
 #set -e
 
+readonly KIBRARY_DIR=$HOME/.Kibrary
+readonly KIBRARY_BIN=$KIBRARY_DIR/bin
+
+while getopts f OPT
+do
+  case $OPT in
+    "f" ) readonly FLG_F="TRUE" 
+  esac
+done
+
+if [ $FLG_F ]; then
+  rm -rf $KIBRARY_DIR
+fi
+
+if [ ! -e $KIBRARY_DIR ]; then
+  mkdir -p $KIBRARY_BIN
+  if [ $? -ne 0 ]; then
+    echo "Could not create $installDir"
+    exit 1 
+  fi
+else
+  echo  "$KIBRARY_DIR already exists. If you want to do a clean install, please add an option -f"
+  exit 2
+fi
+
+cd $KIBRARY_DIR
+
+#bin
+wget -P $KIBRARY_BIN http://kensuke1984.github.io/bin/javaCheck && chmod +x $KIBRARY_BIN/javaCheck
+wget -P $KIBRARY_BIN http://kensuke1984.github.io/bin/anisotime && chmod +x $KIBRARY_BIN/anisotime
+wget -P $KIBRARY_BIN http://kensuke1984.github.io/bin/javaCheck.jar && chmod +x $KIBRARY_BIN/javaCheck.jar
+
+
+$KIBRARY_BIN/javaCheck 
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+#Build Kibrary
+echo "Downloading build scripts in $KIBRARY_DIR"
 wget http://kensuke1984.github.io/gradlew.tar
 tar xf gradlew.tar
 ./gradlew
 ./gradlew build
 
-installDir=$HOME/.Kibrary
-binDir=$installDir/bin
-rm -rf $installDir
-
-if [ ! -e $installDir ]; then
-mkdir -p $binDir 
-if [ $? -ne 0 ]; then
- echo "Could not create $installDir"
-  exit 1 
-fi
-else
- echo  "$installDir already exists"
-exit 2
-fi
-from="build/libs"
-
-file=$(ls $from/kibrary*jar | sed "s/build\/libs\///")
-mv $from/$file $binDir
-
-#bin
-wget -P $binDir http://kensuke1984.github.io/bin/javaCheck && chmod +x $binDir/javaCheck
-wget -P $binDir http://kensuke1984.github.io/bin/anisotime && chmod +x $binDir/anisotime
-wget -P $binDir http://kensuke1984.github.io/bin/javaCheck.jar && chmod +x $binDir/javaCheck.jar
-
-
+mv build/libs/kibrary*jar $KIBRARY_BIN
 
 #bash
-cat <<EOF > $binDir/init_bash.sh
+cat <<EOF >$KIBRARY_BIN/init_bash.sh
 ##classpath
-export CLASSPATH=\$CLASSPATH:$binDir/$file
-export PATH=\$PATH:$binDir
+export CLASSPATH=\$CLASSPATH:$KIBRARY_BIN/$file
+export PATH=\$PATH:$KIBRARY_BIN
 EOF
 
 #tcsh
-cat <<EOF > $binDir/init_tcsh.sh
+cat <<EOF >$KIBRARY_BIN/init_tcsh.sh
 ##classpath
 if ! \$?CLASSPATH then
-    setenv CLASSPATH $binDir/$file
+    setenv CLASSPATH $KIBRARY_BIN/$file
 else
-setenv CLASSPATH \${CLASSPATH}:$binDir/$file
+setenv CLASSPATH \${CLASSPATH}:$KIBRARY_BIN/$file
 endif
-setenv PATH \${PATH}:$binDir
+setenv PATH \${PATH}:$KIBRARY_BIN
 exit 0
 EOF
 
-source $binDir/init_bash.sh 2> /dev/null || source $binDir/init_tcsh.sh 2> /dev/null
+source $KIBRARY_BIN/init_bash.sh 2>/dev/null || source $KIBRARY_BIN/init_tcsh.sh 2>/dev/null
 
 #exit 0
 
