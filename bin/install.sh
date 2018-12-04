0f771f2c8b45746bc8713fdccde869547791508d
kensuke1984 <kensuke@earth.sinica.edu.tw>
2018-12-04 15:08:45 +0800

    install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -2,21 +2,30 @@
 #set -e
 set -o posix
 
-readonly KIBRARY_HOME="$HOME"/.Kibrary
-readonly KIBRARY_BIN="$KIBRARY_HOME"/bin
-readonly KIBRARY_SHARE="$KIBRARY_HOME"/share
+readonly DEFAULT_KIBRARY_HOME="$HOME"/.Kibrary
 
+printf "Where would you like to install Kibrary? (%s) " "$DEFAULT_KIBRARY_HOME"
+read -r KIBRARY_HOME
+if [ -z "$KIBRARY_HOME" ];then
+  KIBRARY_HOME="$DEFAULT_KIBRARY_HOME"
+fi
+KIBRARY_HOME=$(readlink -f "$KIBRARY_HOME")
+printf "Installing in %s... ok? (y/N) " "$KIBRARY_HOME"
+read -r yn
+case "$yn" in [yY]*)  ;; *) echo "Cancelled." ; exit ;; esac
 
 while getopts f OPT
 do
   case $OPT in
     "f" ) readonly FLG_F="TRUE" ;; 
+    *) printf "Invalid options detected.\n"
+      exit 250 ;;
   esac
 done
 
-if which curl >/dev/null 2>&1; then
+if command -v curl >/dev/null 2>&1; then
   downloader=curl
-elif which wget >/dev/null 2>&1; then
+elif command -v wget >/dev/null 2>&1; then
   downloader=wget
 else
   printf "No downloader is found. Install \\e[31mGNU Wget\\e[m (\\e[4mhttps://www.gnu.org/software/wget/\\e[m) or \\e[31mcurl\\e[m (\\e[4mhttps://curl.haxx.se/\\e[m), otherwise please download the latest Kibrary manually.\\n"
@@ -30,9 +39,7 @@ fi
 githubio='https://kensuke1984.github.io'
 gitbin="$githubio/bin"
 if [ ! -e "$KIBRARY_HOME" ]; then
-  mkdir -p "$KIBRARY_BIN"
-  mkdir "$KIBRARY_SHARE"
-  if [ $? -ne 0 ]; then
+  if ! mkdir -p "$KIBRARY_HOME" >/dev/null 2>&1; then
     echo "Could not create $KIBRARY_HOME"
     return 2>/dev/null
     exit 1 
@@ -49,50 +56,51 @@ else
 fi
 
 cd "$KIBRARY_HOME" || (echo "Could not cd to $KIBRARY_HOME. Install failure."; exit 1)
+mkdir bin share
 
 #bin
 if [ $downloader = "curl" ]; then
-  curl -s -o "$KIBRARY_BIN"/javaCheck "$gitbin"/javaCheck
-  curl -s -o "$KIBRARY_BIN"/javaInstall "$gitbin"/javaInstall
-  curl -s -o "$KIBRARY_BIN"/anisotime "$gitbin"/anisotime
-  curl -s -o "$KIBRARY_BIN"/javaCheck.jar "$gitbin"/javaCheck.jar
-  curl -s -o "$KIBRARY_BIN"/.kibraryrc "$gitbin"/kibraryrc
-  curl -s -o "$KIBRARY_BIN"/kibrary_property "$gitbin"/kibrary_property
-  curl -s -o "$KIBRARY_BIN"/kibrary_operation "$gitbin"/kibrary_operation
-  curl -s -o "$KIBRARY_BIN"/oracle_javase_url "$gitbin"/oracle_javase_url
+  curl -s -o bin/javaCheck "$gitbin"/javaCheck
+  curl -s -o bin/javaInstall "$gitbin"/javaInstall
+  curl -s -o bin/anisotime "$gitbin"/anisotime
+  curl -s -o bin/javaCheck.jar "$gitbin"/javaCheck.jar
+  curl -s -o bin/.kibraryrc "$gitbin"/kibraryrc
+  curl -s -o bin/kibrary_property "$gitbin"/kibrary_property
+  curl -s -o bin/kibrary_operation "$gitbin"/kibrary_operation
+  curl -s -o bin/oracle_javase_url "$gitbin"/oracle_javase_url
 else
-  wget -q -P "$KIBRARY_BIN" "$gitbin"/javaCheck
-  wget -q -P "$KIBRARY_BIN" "$gitbin"/javaInstall
-  wget -q -P "$KIBRARY_BIN" "$gitbin"/anisotime
-  wget -q -P "$KIBRARY_BIN" "$gitbin"/javaCheck.jar
-  wget -q -P "$KIBRARY_BIN" "$gitbin"/kibrary_property
-  wget -q -P "$KIBRARY_BIN" "$gitbin"/kibrary_operation
-  wget -q -P "$KIBRARY_BIN" "$gitbin"/oracle_javase_url
-  wget -q -O "$KIBRARY_BIN"/.kibraryrc "$gitbin"/kibraryrc
+  wget -q -P bin "$gitbin"/javaCheck
+  wget -q -P bin "$gitbin"/javaInstall
+  wget -q -P bin "$gitbin"/anisotime
+  wget -q -P bin "$gitbin"/javaCheck.jar
+  wget -q -P bin "$gitbin"/kibrary_property
+  wget -q -P bin "$gitbin"/kibrary_operation
+  wget -q -P bin "$gitbin"/oracle_javase_url
+  wget -q -O bin/.kibraryrc "$gitbin"/kibraryrc
 fi
 
-chmod +x "$KIBRARY_BIN/javaCheck"
-chmod +x "$KIBRARY_BIN/javaInstall"
-chmod +x "$KIBRARY_BIN/anisotime"
-chmod +x "$KIBRARY_BIN/javaCheck.jar"
-chmod +x "$KIBRARY_BIN/kibrary_property"
-chmod +x "$KIBRARY_BIN/kibrary_operation"
-chmod +x "$KIBRARY_BIN/oracle_javase_url"
+chmod +x "bin/javaCheck"
+chmod +x "bin/javaInstall"
+chmod +x "bin/anisotime"
+chmod +x "bin/javaCheck.jar"
+chmod +x "bin/kibrary_property"
+chmod +x "bin/kibrary_operation"
+chmod +x "bin/oracle_javase_url"
 
-if ! "$KIBRARY_BIN"/javaCheck -r >/dev/null 2>&1; then
-  if ! "${KIBRARY_BIN}/javaInstall" -f >/dev/null 2>&1; then
+if ! bin/javaCheck -r >/dev/null 2>&1; then
+  if ! "bin/javaInstall" -f >/dev/null 2>&1; then
     echo "Java is not found and cannot be installed. ANISOtime installation cancelled."
     exit 1
   fi
 fi
 
-if ! "$KIBRARY_BIN"/javaCheck >/dev/null 2>&1; then
-  if ! "${KIBRARY_BIN}/javaInstall" -f >/dev/null 2>&1; then
+if ! bin/javaCheck >/dev/null 2>&1; then
+  if ! "bin/javaInstall" -f >/dev/null 2>&1; then
     echo "Due to a failure of building Kibrary, downloading the latest binary release.";
     if [ $downloader = "curl" ]; then
-      curl -s -o "$KIBRARY_BIN/kibrary-latest.jar" "$gitbin"/kibrary-latest.jar
+      curl -s -o "bin/kibrary-latest.jar" "$gitbin"/kibrary-latest.jar
     else
-      wget -q -P "$KIBRARY_BIN" "$gitbin"/kibrary-latest.jar
+      wget -q -P bin "$gitbin"/kibrary-latest.jar
     fi
     exit 1
   fi
@@ -109,34 +117,33 @@ else
 fi
 tar xf gradlew.tar
 ./gradlew --no-daemon -q >/dev/null
-./gradlew --no-daemon -q build 2>/dev/null 
 
-if [ $? -eq 0 ]; then
-  mv build/libs/kibrary*jar "$KIBRARY_BIN" 
+if ./gradlew --no-daemon -q build 2>/dev/null; then
+  mv build/libs/kibrary*jar bin 
 else
   echo "Due to a failure of building Kibrary, downloading the latest binary release.";
   if [ $downloader = "curl" ]; then
-    curl -s -o "$KIBRARY_BIN/kibrary-latest.jar" "$githubio"/kibrary-latest.jar
+    curl -s -o "bin/kibrary-latest.jar" "$githubio"/kibrary-latest.jar
   else
-    wget -q -P "$KIBRARY_BIN" "$githubio"/kibrary-latest.jar
+    wget -q -P bin "$githubio"/kibrary-latest.jar
   fi
 fi 
 
-readonly KIBRARY=$(ls "$KIBRARY_BIN"/kib*jar)
+readonly KIBRARY=$(ls bin/kib*jar)
 
 #bash
-cat <<EOF >"$KIBRARY_BIN"/init_bash.sh
+cat <<EOF >bin/init_bash.sh
 ##classpath
 export CLASSPATH=\$CLASSPATH:$KIBRARY
-export PATH=\$PATH:$KIBRARY_BIN
-if [ -e "${KIBRARY_HOME}"/java/latest/bin ];then
-  export PATH="${KIBRARY_HOME}"/java/latest/bin:\$PATH
-  export JAVA_HOME="${KIBRARY_HOME}"/java/latest
+export PATH=\$PATH:${KIBRARY_HOME}/bin
+if [ -e ${KIBRARY_HOME}/java/latest/bin ];then
+  export PATH=${KIBRARY_HOME}/java/latest/bin:\$PATH
+  export JAVA_HOME=${KIBRARY_HOME}/java/latest
 fi
 EOF
 
 #tcsh
-cat <<EOF >"$KIBRARY_BIN"/init_tcsh.sh
+cat <<EOF >"$KIBRARY_HOME/bin/init_tcsh.sh"
 ##classpath
 set opt_set = \$?nonomatch
 set nonomatch
@@ -145,7 +152,7 @@ if ! \$?CLASSPATH then
 else
   setenv CLASSPATH \${CLASSPATH}:$KIBRARY
 endif
-setenv PATH \${PATH}:$KIBRARY_BIN
+setenv PATH \${PATH}:$KIBRARY_HOME/bin
 if (\$opt_set == 0) then
   unset nonomatch
 endif
@@ -154,11 +161,11 @@ EOF
 echo Copy and paste the below line to setup PATH and CLASSPATH.
 
 if echo "$SHELL" | grep -qE 'bash|zsh'; then
-  echo "source $KIBRARY_BIN/init_bash.sh"
+  echo "source $KIBRARY_HOME/bin/init_bash.sh"
 elif echo "$SHELL" | grep -qE 'tcsh'; then
-  echo "source $KIBRARY_BIN/init_tcsh.sh" 
+  echo "source $KIBRARY_HOME/bin/init_tcsh.sh" 
 else
-  echo "Please add $KIBRARY_BIN in PATH."
+  echo "Please add $KIBRARY_HOME/bin in PATH."
 fi
 #source $KIBRARY_BIN/init_bash.sh 2>/dev/null || source $KIBRARY_BIN/init_tcsh.sh 2>/dev/null
 #return 2> /dev/null
