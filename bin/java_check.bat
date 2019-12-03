01ca0f478346263c513bb18e93fa8832fabc1eba
Kensuke Konishi <kensuke1984@hotmail.co.jp>
2019-12-03 20:10:01 +0800

    read registry

--- bin/java_check.bat
+++ bin/java_check.bat
@@ -1,3 +1,5 @@
 @echo off
-java -version && msg "%username%" You may have Java installed. || msg "%username%" Get the latest Oracle JDK. & start https://www.oracle.com/technetwork/java/javase/downloads/index.html
 
+rem java -version && msg "%username%" You may have Java installed. || msg "%username%" Get the latest Oracle JDK. & start https://www.oracle.com/technetwork/java/javase/downloads/index.html
+
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft" >nul 2>&1 && msg "%username%" You may have Java installed. || (msg "%username%" Get the latest Oracle JDK. & start https://www.oracle.com/technetwork/java/javase/downloads/index.html)
