8de1d181445ca2ea75ec000d7452bbcecebd452f
Kensuke Konishi <kensuke1984@hotmail.co.jp>
2019-12-03 14:15:24 +0800

    simple Java check in Windows.

---
+++ bin/java_check.bat
@@ -0,0 +1,3 @@
+@echo off
+java -version && msg "%username%" You may have Java installed. || msg "%username%" Get the latest Oracle JDK. & start https://www.oracle.com/technetwork/java/javase/downloads/index.html
+
