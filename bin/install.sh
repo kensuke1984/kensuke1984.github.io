69d194886373108a6315676ea73ea239ae9cdfb3
kensuke1984 <kensuke@earth.sinica.edu.tw>
2019-12-17 17:22:09 +0800

    Small intallation

--- bin/install.sh
+++ bin/install.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
+#
+# Installer of Kibrary  Kensuke Konishi
+#
+
 if [ "$(readlink /bin/sh)" != "dash" ];then
   set -o posix
 fi
 
-readonly KIBIN_URL='http://bit.ly/37wxazr'
+readonly install_version='0.2.0'
+readonly KIBIN_URL='https://bit.ly/37wxazr'
+readonly kibrary_jar='kibrary-0.4.5.jar'
+readonly KIBRARY_MASTER_ZIP='https://bit.ly/2qY5J0O'
 readonly DEFAULT_KIBRARY_HOME="$HOME/Kibrary"
 readonly logfile="$(pwd)/kinst.log"
 readonly errfile="$(pwd)/kinst.err"
-readonly install_version='0.1.5'
+readonly githubio='https://kensuke1984.github.io'
+readonly gitbin="$githubio/bin"
 
 #Emulates readlink -f hoge
 __readlink_f (){
@@ -40,6 +48,21 @@ __mvlog(){
   mv "$logfile" "$errfile" "$KIBRARY_HOME"
 }
 
+__download_kibin(){
+  if [ $downloader = "curl" ]; then
+    curl -sL -o "$kibrary_jar" "$KIBIN_URL"
+  else
+    wget -q -O "$kibrary_jar" "$KIBIN_URL"
+  fi
+  __md5 "$kibrary_jar" >>"$logfile"
+}
+
+__unexpected_exit(){
+  printf "$2 ($1)\n" | tee -a "$errfile"
+  __mvlog
+  exit $1
+}
+
 touch $logfile $errfile
 echo "###install.sh stdout $install_version" >>$logfile
 echo "###install.sh stderr $install_version" >>$errfile
@@ -57,34 +80,31 @@ if command -v curl >/dev/null 2>>"$errfile"; then
 elif command -v wget >/dev/null 2>>"$errfile"; then
   downloader=wget
 else
-  printf "No downloader is found. Install \\e[31mGNU Wget\\e[m (\\e[4mhttps://www.gnu.org/software/wget/\\e[m) or \\e[31mcurl\\e[m (\\e[4mhttps://curl.haxx.se/\\e[m), otherwise please download the latest Kibrary manually. (3)\\n" | tee -a "$errfile"
-  exit 3
-fi
+  __unexpected_exit 3 "No downloader is found. Install \\e[31mGNU Wget\\e[m (\\e[4mhttps://www.gnu.org/software/wget/\\e[m) or \\e[31mcurl\\e[m (\\e[4mhttps://curl.haxx.se/\\e[m), otherwise please download the latest Kibrary manually."
+fi 
+
 echo "downloader=$downloader" >>"$logfile"
 
 KIBRARY_HOME="$(__readlink_f "$KIBRARY_HOME")"
 printf "Installing in %s ... ok? (y/N) " "$KIBRARY_HOME"
 read -r yn </dev/tty
-case "$yn" in [yY]*)  ;; *) echo "Installation cancelled. (1)"; exit 1;; esac
+case "$yn" in [yY]*)  ;; *) __unexpected_exit 1 "Installation cancelled.";; esac
 
 echo "KIBRARY_HOME=$KIBRARY_HOME" >>$logfile
 
-githubio='https://kensuke1984.github.io'
-gitbin="$githubio/bin"
 if [ ! -e "$KIBRARY_HOME" ]; then
   if ! mkdir -p "$KIBRARY_HOME" >>"$logfile" 2>>"$errfile"; then
-    echo "Could not create $KIBRARY_HOME. (1)" | tee -a "$errfile"
-    exit 1 
+    __unexpected_exit 1 "Could not create $KIBRARY_HOME."
   fi
 else
   printf "%s exists. Do you want to \\e[4;31mremove\\e[m it and continue? (y/N)\\n" "$KIBRARY_HOME"
   read -r yn </dev/tty
-  case "$yn" in [yY]*)  ;; *) echo "Installation cancelled." ; exit 2;; esac
+  case "$yn" in [yY]*)  ;; *) __unexpected_exit 2 "Installation cancelled.";; esac
   rm -rf "$KIBRARY_HOME"
   mkdir "$KIBRARY_HOME"
 fi
 
-cd "$KIBRARY_HOME" || (echo "Could not cd to $KIBRARY_HOME. Install failure. (1)" | tee -a "$errfile"; exit 1)
+cd "$KIBRARY_HOME" || __unexpected_exit 3 "Could not cd to $KIBRARY_HOME. Installation failure."
 mkdir bin share
 export KIBRARY_HOME
 
@@ -98,118 +118,24 @@ if [ $downloader = "curl" ]; then
 else
   wget -q -O "$catalog_zip" "$piac_html"
 fi
-(cd share; unzip -q "$catalog_zip")
+(cd share && unzip -q "$catalog_zip" || __unexpected_exit 69 "Could not cd to share.")
 rm "$catalog_zip"
 __md5 share/*.cat
 
 #bin
-if [ $downloader = "curl" ]; then
-  curl -s -o bin/javaCheck "$gitbin"/javaCheck
-  curl -s -o bin/javaInstall "$gitbin"/javaInstall
-  curl -s -o bin/anisotime "$gitbin"/anisotime
-  curl -s -o bin/javaCheck.jar "$gitbin"/javaCheck.jar
-  curl -s -o bin/readlink_f.sh "$gitbin"/readlink_f.sh
-  curl -s -o bin/kibrary_property "$gitbin"/kibrary_property
-  curl -s -o bin/kibrary_operation "$gitbin"/kibrary_operation
-  curl -s -o bin/oracle_javase_url "$gitbin"/oracle_javase_url
-#  curl -s -o bin/.kibraryrc "$gitbin"/kibraryrc
-else
-  wget -q -P bin "$gitbin"/javaCheck
-  wget -q -P bin "$gitbin"/javaInstall
-  wget -q -P bin "$gitbin"/anisotime
-  wget -q -P bin "$gitbin"/readlink_f.sh
-  wget -q -P bin "$gitbin"/javaCheck.jar
-  wget -q -P bin "$gitbin"/kibrary_property
-  wget -q -P bin "$gitbin"/kibrary_operation
-  wget -q -P bin "$gitbin"/oracle_javase_url
-#  wget -q -O bin/.kibraryrc "$gitbin"/kibraryrc
-fi
-
-__md5 bin/javaCheck
-__md5 bin/javaInstall
-__md5 bin/anisotime
-__md5 bin/readlink_f.sh
-__md5 bin/javaCheck.jar
-__md5 bin/kibrary_property
-__md5 bin/kibrary_operation
-__md5 bin/oracle_javase_url
-
-chmod +x bin/javaCheck
-chmod +x bin/javaInstall
-chmod +x bin/anisotime
-chmod +x bin/readlink_f.sh
-chmod +x bin/javaCheck.jar
-chmod +x bin/kibrary_property
-chmod +x bin/kibrary_operation
-chmod +x bin/oracle_javase_url
-
-if [ -z "$JAVA" ];then
-  JAVA='java'
-  echo "JAVA is set to be 'java'. ($(command -v java))" >>$logfile
-else
-  echo "JAVA=$JAVA ($(command -v $JAVA))" >>$logfile
-fi
-
-if [ -z "$JAVAC" ];then
-  JAVAC='javac'
-  echo "JAVAC is set to be 'javac' ($(command -v javac))" >>$logfile
-else
-  echo "JAVAC=$JAVAC ($(command -v $JAVAC))" >>$logfile
-fi
-export JAVA JAVAC
-
-./bin/javaCheck -v >>"$logfile" 2>>"$errfile"
-
-bin/javaCheck -r >>"$logfile" 2>>"$errfile"
-if [ $? -ge 20 ] ; then
-  echo "Java is not found. ANISOtime installation cancelled. (71)" | tee -a "$errfile"
-  __mvlog
-  exit 71
-fi
-
-bin/javaCheck >>"$logfile" 2>>"$errfile"
-if [ $? -ge 20 ] ; then
-  echo "Because you do not have a Java compiler installed, downloading the latest binary release. (81)" | tee -a "$errfile"
-  kibpath='bin/kibrary-0.4.5.jar'
+for binfile in anisotime readlink_f.sh kibrary_property kibrary_operation
+do
   if [ $downloader = "curl" ]; then
-    curl -sL -o "$kibpath" "$KIBIN_URL"
+    curl -s -o "bin/$binfile" "$gitbin/$binfile"
   else
-    wget -q -O "$kibpath" "$KIBIN_URL"
+    wget -q -P bin "$gitbin/$binfile"
   fi
-  __md5 "$kibpath"
-  __mvlog
-  exit 81
-fi
-
-#Build Kibrary
-echo "Kibrary is in $KIBRARY_HOME" | tee -a "$logfile"
-if [ $downloader = "curl" ]; then
-  curl -s -o gradlew.tar "$githubio"/gradlew.tar
-else
-  wget -q "$githubio"/gradlew.tar
-fi
-__md5 gradlew.tar
-tar xf gradlew.tar
-./gradlew -q --no-daemon >/dev/null 2>&1
-
-if ./gradlew -q --no-daemon build >/dev/null 2>&1; then
-  mv build/libs/kibrary*jar bin 
-else
-  echo "Due to a failure of building Kibrary, downloading the latest binary release. (82)" | tee -a "$errfile"
-  kibpath='bin/kibrary-0.4.5.jar'
-  if [ $downloader = "curl" ]; then
-    curl -sL -o "$kibpath" "$KIBIN_URL" 2>>"$errfile"
-  else
-    wget -q -O "$kibpath" "$KIBIN_URL" 2>>"$errfile"
-  fi
-  __md5 "$kibpath"
-  __mvlog
-  exit 82
-fi 
-
-__md5 "$(__readlink_f bin/kib*jar)"
-
+  __md5 "bin/$binfile"
+  chmod +x "bin/$binfile"
+done
+(cd bin && __download_kibin || __unexpected_exit 31 "Could not download the Kibrary.")
 __mvlog
+
 exit 0
 
 
