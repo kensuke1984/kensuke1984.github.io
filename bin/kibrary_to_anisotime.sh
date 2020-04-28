85ed4f41a1d34bb4344c9904add77494d550df9e
kensuke1984 <kensuke@earth.sinica.edu.tw>
2020-04-28 15:06:08 +0800

    k2a

---
+++ bin/kibrary_to_anisotime.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+k2a_version='0.0.1'
+cwd=$(pwd)
+file="$cwd/$1"
+name="anisotime-1.3.7.jar"
+test -f "$file" || exit 71
+test -f "$name" && exit 2
+dir=$(mktemp -d)
+cd "$dir" || exit 1
+jar xf "$file"
+sed -i 's/kibrary.About/anisotime.ANISOtime/' META-INF/MANIFEST.MF
+jar cmf META-INF/MANIFEST.MF "$name" ./*
+mv "$name" "$cwd"
+rm -rf "$dir"
