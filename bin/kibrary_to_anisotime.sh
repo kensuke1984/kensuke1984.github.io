c5f7618cfd91c4b7467598cb38411ec2109e7330
kensuke1984 <kensuke@earth.sinica.edu.tw>
2020-05-03 17:07:08 +0800

    anisotime

--- bin/kibrary_to_anisotime.sh
+++ bin/kibrary_to_anisotime.sh
@@ -1,11 +1,39 @@
 #!/bin/sh
 
-k2a_version='0.0.2'
+k2a_version='0.0.3'
+
+#Emulates readlink -f hoge
+__readlink_f (){
+  TARGET_FILE=$1
+  if [ "$(echo "$TARGET_FILE" | cut -c 1-2)" = "~/" ]; then
+    TARGET_FILE=$HOME/${TARGET_FILE#\~/}
+  fi
+  while [ "$TARGET_FILE" != "" ];
+  do
+    cd "$(dirname "$TARGET_FILE")" || exit 60
+    FILENAME="$(basename "$TARGET_FILE")"
+    TARGET_FILE="$(readlink "$FILENAME")"
+  done
+  if [ "$FILENAME" = "." ]; then
+    echo "$(pwd -P)"
+  else
+    echo "$(pwd -P)/$FILENAME"
+  fi
+}
+
 cwd=$(pwd)
-file="$(readlink -f $1)"
-name="anisotime-1.3.7.jar"
-test -f "$file" || exit 71
-test -f "$name" && exit 2
+file="$(__readlink_f $1)"
+if [ ! -f "$file" ]; then
+  printf "%s does not exist.\n" "$file" 1>&2
+  exit 71
+fi
+
+anisotime_version=$(java -cp $file -Djava.awt.headless=true io.github.kensuke1984.anisotime.About | head -1 | awk '{print $2}')
+name="anisotime-${anisotime_version%.*}.jar"
+if [ -f "$name" ]; then
+  printf "%s already exists.\n" "$name" 1>&2
+  exit 2
+fi
 dir=$(mktemp -d)
 cd "$dir" || exit 1
 jar xf "$file"
